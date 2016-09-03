de9c4f63bafc01facbf5ffab6e8211e01d49697d
matts1 <mattstark75@gmail.com>
2016-09-03 12:31:39 +1000

    Forgot to add the cpp files before (just deleted the .c files). Fixed that

--- apps/sos/include/internal/fs/NFSFileSystem.h
+++ apps/sos/include/internal/fs/NFSFileSystem.h
@@ -20,6 +20,12 @@ public:
 
     virtual ~NFSFileSystem() override = default;
 
+    // can't copy or move. This way it can be passed as a reference to the lambda functions
+    NFSFileSystem(const NFSFileSystem&) = delete;
+    NFSFileSystem(NFSFileSystem&&) = delete;
+    NFSFileSystem& operator=(const NFSFileSystem&) = delete;
+    NFSFileSystem& operator=(NFSFileSystem&&) = delete;
+
     virtual boost::future <std::shared_ptr<File>> open(const std::string &pathname) override;
 
     boost::future <std::shared_ptr<File>> lookup(std::string path);
--- apps/sos/src/fs/NFSFileSystem.cpp
+++ apps/sos/src/fs/NFSFileSystem.cpp
@@ -1,7 +1,6 @@
 #include "internal/fs/NFSFileSystem.h"
 #include <memory>
 
-#define NFS_CALLBACK(lambda, ...) make_unique<std::function<void(uintptr_t, ...)>>(lambda)}
 
 namespace fs {
 
@@ -11,22 +10,22 @@ NFSFileSystem::NFSFileSystem() {
 }
 
 boost::future<std::shared_ptr<File>> NFSFileSystem::lookup(std::string path) {
-    boost::promise<std::shared_ptr<File>> promise;
+    auto promise = std::make_shared<boost::promise<std::shared_ptr<File>>>();
     if (lookupCache.find(path) != lookupCache.end())
-        promise.set_value(lookupCache.find(path)->second);
+        promise->set_value(lookupCache.find(path)->second);
     else
-        auto lambda = [=](uintptr_t, enum nfs_stat status, fhandle_t *fh, fattr_t *fattr) {
-            if (status == NFS_OK && fattr->type == NFNON) {
-                // sos files can only be regular files
-                lookupCache.emplace(path, std::make_shared<File>(fh, fattr));
-                promise.set_value(lookupCache.find(path)->second);
-            } else
-                promise.set_value(nullptr);
-            printf("Got status %d (NFS_OK = %d)\n", status, NFS_OK);
-        };
-        nfs_lookup(&mnt_point, path.c_str(),
-                   NFS_CALLBACK(lambda, enum nfs_stat, fhandle_t*, fattr_t*), 0);
-    return promise.get_future();
+        nfs_lookup(&mnt_point, path.c_str(), std::make_unique<nfs_lookup_cb_t>(
+                [this, promise, path](uintptr_t, enum nfs_stat status, fhandle_t *fh, fattr_t *fattr) {
+                    if (status == NFS_OK && fattr->type == NFNON) {
+                        // sos files can only be regular files
+                        this->lookupCache.emplace(path, std::make_shared<File>(fh, fattr));
+                        promise->set_value(this->lookupCache.find(path)->second);
+                    } else
+                        promise->set_value(nullptr);
+                    printf("Got status %d (NFS_OK = %d)\n", status, NFS_OK);
+                }
+        ), 0);
+    return promise->get_future();
 }
 
 }
